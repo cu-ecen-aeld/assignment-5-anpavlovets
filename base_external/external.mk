716f28e9587ef7610a13214311dd8236a34f3a32
Alexander Pavlovets <pavlovets_edu@mail.ru>
2022-08-01 10:02:22 +0300

    resolving conflicts-2

--- base_external/external.mk
+++ base_external/external.mk
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 include $(BR2_EXTERNAL_project_base_PATH)/package/aesd-assignments/aesd-assignments.mk
-=======
-include $(BR2_EXTERNAL_project_base_PATH)/package/aesd-assignments/aesd-assignments.mk
->>>>>>> 654677d483ef6b9b12d75c01f45f228091474e89
